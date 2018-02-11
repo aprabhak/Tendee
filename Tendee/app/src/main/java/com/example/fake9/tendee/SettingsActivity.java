668662b58c0f3b6899028de855060f7b067ca334
Avinash <aprabhak@purdue.edu>
2018-02-10 21:13:44 -0500

    change password

--- Tendee/app/src/main/java/com/example/fake9/tendee/SettingsActivity.java
+++ Tendee/app/src/main/java/com/example/fake9/tendee/SettingsActivity.java
@@ -1,12 +1,18 @@
 package com.example.fake9.tendee;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -26,6 +32,7 @@ public class SettingsActivity extends AppCompatActivity {
     private TextView mAddress;
     private TextView mEditDescription;
     private TextView mEditAddress;
+    private Button mEditPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,7 @@ public class SettingsActivity extends AppCompatActivity {
         mAddress = (TextView)findViewById(R.id.settings_address);
         mEditDescription = (TextView)findViewById(R.id.settings_edit_description);
         mEditAddress = (TextView)findViewById(R.id.settings_edit_address);
+        mEditPassword = (Button)findViewById(R.id.EditPassBtn);
 
         mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
         String current_uid = mCurrentUser.getUid();
@@ -79,6 +87,47 @@ public class SettingsActivity extends AppCompatActivity {
                 Intent editIntent = new Intent(SettingsActivity.this,EditSettingsActivity.class);
                 editIntent.putExtra("edit","address");
                 startActivity(editIntent);
+            }
+        });
+
+        mEditPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(SettingsActivity.this, "yoyo", Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(SettingsActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.change_password,null);
+                final EditText currentPassword = (EditText)mView.findViewById(R.id.edcurrPassword);
+                final EditText newPassword = (EditText)mView.findViewById(R.id.ednewPassword);
+                final EditText newPassword2 = (EditText)mView.findViewById(R.id.ednewPassword2);
+                Button changePassBtn = (Button)mView.findViewById(R.id.changePassword);
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.show();
+                changePassBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String currentPass = currentPassword.getText().toString();
+                        String newPass = newPassword.getText().toString();
+                        String newPass2 = newPassword2.getText().toString();
+                        if (!newPass.equals(newPass2)) {
+                            Toast.makeText(SettingsActivity.this, "new passwords do not match", Toast.LENGTH_SHORT).show();
+                            dialog.dismiss();
+                            return;
+                        }
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        user.updatePassword(newPass).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(SettingsActivity.this, "password updated", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(SettingsActivity.this, "failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                        dialog.dismiss();
+                    }
+                });
             }
         });
 
