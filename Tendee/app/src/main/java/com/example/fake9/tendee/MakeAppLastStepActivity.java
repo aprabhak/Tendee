be1fef5d98edb7e3ea7fa5558b990e7b6feb2fda
chaolun shao <ChaoLun@chaolundeMacBook-Air.local>
2018-03-09 10:24:13 -0500

    Newest add to the table done

---
+++ Tendee/app/src/main/java/com/example/fake9/tendee/MakeAppLastStepActivity.java
@@ -0,0 +1,13 @@
+package com.example.fake9.tendee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MakeAppLastStepActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_app_last_step);
+    }
+}
---
+++ Tendee/app/src/main/java/com/example/fake9/tendee/Result_AttendeeActivity.java
@@ -0,0 +1,13 @@
+package com.example.fake9.tendee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Result_AttendeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_attendee);
+    }
+}
